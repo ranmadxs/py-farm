6b890be7ecee866aed8e94ddf9f70a0a9d9aa951
Edgar Sanchez <esanchez@sigma.cl>
2016-03-23 12:04:26 -0300

    fswebcam

--- svc/Camera.py
+++ svc/Camera.py
@@ -3,27 +3,34 @@ Created on 22-03-2016
 
 @author: esanchez
 '''
-import pygame.camera
-import pygame.image
+#import pygame.camera
+#import pygame.image
 from time import gmtime, strftime
 import os
 
 FOLDER_WEBCAM = '/tmp/motion/'
+CAMERA_DEVICE = '/dev/video0'
 PIC_WIDTH = 320
 PIC_HEIGHT = 240
-
+#fswebcam -d /dev/video0 -r 640x480 test.jpg
 class WebCam():
-    def capturarDatos(self):
+    def capturarDatos(self):        
+        
         if not os.path.exists(FOLDER_WEBCAM):
-            os.makedirs(FOLDER_WEBCAM)        
-        pygame.camera.init()
+            os.makedirs(FOLDER_WEBCAM)
+        print "Usando camara %s ..." % CAMERA_DEVICE
+        fileTime = strftime("%Y-%m-%d_%H:%M:%S", gmtime())
+        fileName = '%spic_%s.jpg'%(FOLDER_WEBCAM, fileTime)    
+        os.system('fswebcam -d %s -r %dx%d %s' % (CAMERA_DEVICE, PIC_WIDTH, PIC_HEIGHT, fileName))  
+        
+'''        pygame.camera.init()
         cameras = pygame.camera.list_cameras()
         print "Usando camara %s ..." % cameras[0]
         webcam = pygame.camera.Camera(cameras[0], (PIC_WIDTH, PIC_HEIGHT))
         webcam.start()
         image= webcam.get_image()
-        fileTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-        fileName = '%spic %s.jpg'%(FOLDER_WEBCAM, fileTime)
+
         print 'Guardando imagen %s'%fileName
         pygame.image.save(image, fileName)
         webcam.stop()
+'''
