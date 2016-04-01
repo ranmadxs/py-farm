9c13543219ccf2def0620627a244bbbf0a87b616
Edgar Sanchez <esanchez@sigma.cl>
2016-03-31 23:02:27 -0300

    Arreglando log y quitando sensor de humedad tierra

--- libs/log.py
+++ libs/log.py
@@ -5,6 +5,22 @@ Created on 30-03-2016
 '''
 import logging
 
-logging.basicConfig()
+#logging.basicConfig()
 log = logging.getLogger("pyfarm")
 log.setLevel(logging.DEBUG)
+
+
+# create console handler and set level to debug
+logHandler = logging.StreamHandler()
+logHandler.setLevel(logging.DEBUG)
+
+# create formatter
+logFormatter = logging.Formatter("%(asctime)s [%(levelname)s]  %(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+# add formatter to ch
+logHandler.setFormatter(logFormatter)
+
+# add handler to logger
+log.addHandler(logHandler)
+
+log.info("Init loggin pyfarm")
